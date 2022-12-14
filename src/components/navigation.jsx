42fbf98411176538711ffa74159fa13af26c49d8
Dim43a <dim43a@gmail.com>
2022-12-14 20:47:59 +0200

    commit

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -48,13 +48,13 @@ export const Navigation = () => {
                 >
                     <ul className='nav navbar-nav navbar-right'>
                         <li>
-                            <a href='#features' className='page-scroll'>
-                                Features
+                            <a href='#about' className='page-scroll'>
+                                About
                             </a>
                         </li>
                         <li>
-                            <a href='#about' className='page-scroll'>
-                                About
+                            <a href='#features' className='page-scroll'>
+                                Features
                             </a>
                         </li>
                         <li>
